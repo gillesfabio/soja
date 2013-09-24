2006a4f667c2a01ec598a446f0e014f1aeeab504
Gilles Fabio <gilles@gillesfabio.com>
2013-09-24 18:53:11 +0200

    Add MessageCollection and MessageModel classes.

---
+++ app/collections/MessageCollection.js
@@ -0,0 +1,13 @@
+define([
+
+	'backbone'
+
+], function(Backbone) {
+
+	'use strict';
+
+	var MessageCollection = Backbone.Collection.extend({});
+
+	return MessageCollection;
+
+});
---
+++ app/models/MessageModel.js
@@ -0,0 +1,13 @@
+define([
+
+	'backbone'
+
+], function(Backbone) {
+
+	'use strict';
+
+	var MessageModel = Backbone.Model.extend({});
+
+	return MessageModel;
+
+});
